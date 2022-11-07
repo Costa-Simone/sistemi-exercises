6b90e3a357c7f1141a60a28d1ea035ebbddbb47f
Costa Simone <s.costa.2235@vallauri.edu>
2022-11-07 09:22:19 +0100

    Continuazione esercizio AllocazioneDinamica02

--- QUARTA/Allocazione dinamica/Ex02 Allocazione dinamica/main.c
+++ QUARTA/Allocazione dinamica/Ex02 Allocazione dinamica/main.c
@@ -12,18 +12,24 @@ void stampaVett(int *v, int *len);
 int copiaPari(int *v1, int *v2, int *len);
 void InvertiStringa(char *vC);
 void ConcatenaStringa(char *s, char *s2);
+void spostaCaratteriUnivoci(char *vC, char *vC2);
+void splittaStringa(char *s, char *s2, int *n, char *sep);
 
 int main() {
     char *scelta;
     int *len, *len2, *lenS;
-    int *vI;
-    int *vI2;
+    int *vI, *vI2;
+    int *campo;
     char *vC, *vC2;
+    char *separatore;
 
     len = (int*) calloc(1, sizeof(int));
     len2 = (int*) calloc(1, sizeof(int));
     lenS = (int*) calloc(1, sizeof(int));
+    campo = (int*) calloc(1, sizeof(int));
+    separatore = (char*) malloc(sizeof(char));
     *lenS = 51;
+    *separatore = ';';
 
     srand(time(NULL));
 
@@ -35,7 +41,7 @@ int main() {
 
         switch (*scelta) {
             case 'a':
-            case 'A':
+            case 'A': // Dato un vettore vI interi casuali caricare in v2 solo i valori pari
                 printf("\nInserisci la lunghezza del vettore: ");
                 scanf("%d", len);
 
@@ -47,12 +53,14 @@ int main() {
                 *len2 = copiaPari(vI, vI2, len);
 
                 stampaVett(vI2, len2);
+                fflush(stdin);
+                getchar();
                 free(vI);
                 free(vI2);
                 break;
 
             case 'b':
-            case 'B':
+            case 'B': // Inverti contenuto stringa
                 fflush(stdin);
                 getchar();
 
@@ -69,7 +77,7 @@ int main() {
                 break;
 
             case 'c':
-            case 'C':
+            case 'C': // Concatena stringa s1 ed s2
                 vC = (char*) malloc(MAX * sizeof(char));
                 vC2 = (char*) malloc(MAX * sizeof(char));
 
@@ -82,14 +90,41 @@ int main() {
                 printf("Stringa concatenata --> %s", vC);
                 free(vC);
                 free(vC2);
+                getchar();
                 break;
 
             case 'd':
-            case 'D':
+            case 'D': // Sposta in stringa s2 solo caratteri che non si ripetono in s1
+                vC = (char*) malloc(MAX * sizeof(char));
+                vC2 = (char*) malloc(MAX * sizeof(char));
+
+                printf("Inserisci la stringa: ");
+                gets(vC);
+                fflush(stdin);
+                spostaCaratteriUnivoci(vC, vC2);
+                printf("\nLa stringa dei caratteri univoci: %s", vC2);
+                free(vC);
+                free(vC2);
+                getchar();
                 break;
 
             case 'e':
-            case 'E':
+            case 'E': // Splitta stringa
+                vC = (char*) malloc(MAX * sizeof(char));
+                vC2 = (char*) malloc(MAX * sizeof(char));
+
+                printf("Inserisci la stringa da splittare(con separatore avente ';'): ");
+                gets(vC);
+                fflush(stdin);
+                printf("Inserisci il numero del campo desiderato: ");
+                scanf("%d", campo);
+                (*campo)--;
+                splittaStringa(vC, vC2, campo, separatore);
+                printf("\nIl campo desiderato e': %s", vC2);
+                fflush(stdin);
+                free(vC);
+                free(vC2);
+                getchar();
                 break;
         }
     } while (*scelta != 'q' && *scelta != 'Q');
@@ -97,17 +132,79 @@ int main() {
     return 0;
 }
 
+void splittaStringa(char *s, char *s2, int *n, char *sep) {
+    int *cnt, *i;
+
+    cnt = (int*) malloc(sizeof(int));
+    i = (int*) malloc(sizeof(int));
+    *cnt = 0;
+    *i = 0;
+
+    while (*cnt != *n) {
+        while(*(s + *i) != *sep) {
+            (*i)++;
+        }
+
+        (*cnt)++;
+        (*i)++;
+    }
+
+    *cnt = 0;
+
+    while (*(s + *i) != *sep && *(s + *i) != '\0') {
+        *(s2 + *cnt) = *(s + *i);
+        (*i)++;
+        (*cnt)++;
+    }
+
+    *(s2 + *cnt) = '\0';
+}
+void spostaCaratteriUnivoci(char *vC, char *vC2) {
+    char *p, *p2, *scor;
+    int *len, *trovato;
+
+    len = (int*) malloc(sizeof(int));
+    trovato = (int*) malloc(sizeof(int));
+
+    *len = 0;
+    p = vC;
+    p2 = vC2;
+
+    for(; p < (vC + strlen(vC)); p++) {
+        *trovato = 0;
+
+        for(scor = vC; scor < (vC + strlen(vC)); scor++) {
+            if(p != scor) {
+                if(*p == *scor) {
+                    *trovato = 1;
+                }
+            }
+        }
+
+
+        if(*trovato == 0) {
+            *(p2 + *len) = *p;
+            (*len)++;
+        }
+    }
+
+    *(p2 + *len) = '\0';
+}
 void ConcatenaStringa(char *s, char *s2) {
     char *p, *p2;
 
     s = realloc(s, strlen(s) + strlen(s2) + 1);
     p2 = s2;
-    p = strlen(s) + 1;
+    p = s + strlen(s);
+    *p = ' ';
+    p++;
 
-    for(; p2 < (s2 + strlen(s2)); p2++, p++) {
+    for(; p2 < (s2 + strlen(s2)); p2++) {
         *p = *p2;
         p++;
     }
+
+    *p = '\0';
 }
 void InvertiStringa(char *vC) {
     char *aus, *pF;
@@ -136,7 +233,7 @@ int copiaPari(int *v1, int *v2, int *len) {
 
     for(*i = 0; *i < *len; (*i)++) {
         if(*(v1 + (*i)) % 2 == 0) {
-            *(v2 + (*j)) = *v1;
+            *(v2 + (*j)) = *(v1 + *i);
             (*j)++;
         }
     }
